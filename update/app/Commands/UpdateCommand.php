d8515e48ee94eee2075f4a8551dca2623c69df14
mabolek <mathias@lilio.com>
2021-06-05 21:27:13 +0200

    [TASK] Test branch list

--- update/app/Commands/UpdateCommand.php
+++ update/app/Commands/UpdateCommand.php
@@ -113,7 +113,7 @@ class UpdateCommand extends Command
 
         Git::execute(['config', '--local', 'user.name', env('GIT_NAME', 'cu')]);
         Git::execute(['config', '--local', 'user.email', env('GIT_EMAIL', 'cu@composer-update')]);
-
+        $this->info(var_export(Git::getBranches(), true));
         if (!env('APP_SINGLE_BRANCH') || !in_array($this->new_branch, Git::getBranches() ?? [])) {
             $this->info('Creating branch "' . $this->new_branch . '".');
 
