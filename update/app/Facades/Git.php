a8a6ff82b408144b52f7f3aa70bc82c0b6953535
kawax <kawaxbiz@gmail.com>
2023-01-30 09:57:29 +0900

    wip

--- update/app/Facades/Git.php
+++ update/app/Facades/Git.php
@@ -6,11 +6,17 @@ use CzProject\GitPhp\GitRepository;
 use Illuminate\Support\Facades\Facade;
 
 /**
- * @method static string[] execute(...$cmd)
+ * @method static array execute(...$cmd)
  * @method static bool hasChanges()
  * @method static GitRepository setRemoteUrl(string $string, string $string1)
  * @method static GitRepository createBranch(string $new_branch, bool $true)
  * @method static GitRepository addAllChanges()
+ * @method static string getCurrentBranchName()
+ * @method static array getBranches()
+ * @method static GitRepository fetch($remote = null, array $options = null)
+ * @method static GitRepository checkout(string $name)
+ * @method static GitRepository pull($remote = null, array $options = null)
+ * @method static GitRepository merge($branch, $options = null)
  *
  * @mixin GitRepository
  */
